d9eb68a850c60c2f2bc3de89b8eb80cb9b1287b8
Kenza Tighrine <k.tghrn@outlook.fr>
2023-10-19 17:23:35 +0200

    edit firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { firebaseConfig } from "./firebase_config"
+import { firebaseConfig } from "./firebase-config"
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
